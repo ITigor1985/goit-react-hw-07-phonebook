faad4f09679a334501d3bdf3453a1affdc2e9903
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-04-06 21:10:02 +0300

    style

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,19 +1,18 @@
 import styled from 'styled-components';
 import phone from '../images/phone.png';
-import icons from '../images/icons.png';
 
 export const Container = styled.div`
   position: relative;
-  padding-top: 30px;
+
   padding-bottom: 30px;
   width: 100%;
   height: 700px;
   margin: 0 auto;
   background-size: contain;
-  background: url(${icons}), url(${phone});
+  background: url(${phone});
   background-repeat: no-repeat;
-  background-position: center 120px, top;
-  background-size: 260px 400px, 365px;
+  background-position: top;
+  background-size: 365px;
   display: flex;
   flex-direction: column;
   justify-content: center;
